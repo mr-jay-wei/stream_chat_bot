bc340f536f0bc212d28142d8e1e770831217b04e
Jay <854326781@qq.com>
2025-09-18 16:42:58 +0800

    移动端增加.env文件

--- mobile/api/index.js
+++ mobile/api/index.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 
 // 你的电脑在局域网中的IP地址。这是让手机能访问到电脑上运行的后端服务的关键。
 // 你需要将 'YOUR_COMPUTER_IP' 替换成你自己的IP地址。
-const YOUR_COMPUTER_IP = '172.20.10.2';//'192.168.31.134'; // 例如: 
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
-const API_BASE_URL = `http://${YOUR_COMPUTER_IP}:28501/api`;
+if (!API_BASE_URL) {
+  alert('错误：API URL 未在环境变量中配置！请检查 mobile/.env 文件。');
+}
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
